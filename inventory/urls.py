769de760746d9657d9650ae5484a457eeec10a99
Laurent-B2002 <laubia20@gmail.com>
2024-11-12 18:46:46 +0000

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('inventory/', views.get_inventory, name='get_inventory'),  # Get all books as JSON
     path('search/', views.search_books, name='search_books'),   # Search for books as JSON
     path('authors/', views.get_authors, name='get_authors'),         # Get all authors as JSON
+    path('addbooks/', views.add_books, name='add_books'),
     path('addauthors/', views.add_authors, name='add_authors'),
 ]
