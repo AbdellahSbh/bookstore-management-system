82a9e08b8c09bd063696d283813d40157b931a1b
AbdellahSbh <110669982+AbdellahSbh@users.noreply.github.com>
2024-11-14 12:14:40 +0100

    urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inventory/', views.get_inventory, name='get_inventory'),  # Get all books as JSON
-    path('search/', views.search_books, name='search_books'),   # Search for books as JSON
-    path('authors/', views.get_authors, name='get_authors'),         # Get all authors as JSON
+    path('inventory/', views.get_inventory, name='get_inventory'),  
+    path('search/', views.search_books, name='search_books'),   
+    path('authors/', views.get_authors, name='get_authors'),   
     path('addbooks/', views.add_books, name='add_books'),
     path('addauthors/', views.add_authors, name='add_authors'),
+    path('update_book/', views.update_book, name='update_book'),  
+    path('delete_book/', views.delete_book, name='delete_book'),
 ]
